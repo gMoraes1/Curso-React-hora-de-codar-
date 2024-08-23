7d850937a99c9d95e22b8a0840f9d00b7b8b2e54
gMoraes1 <gustavo.correiademoraes2006@gmail.com>
2024-08-22 22:52:31 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 // App.js
 import './App.css';
-import Evento from './components/Evento'; // Certifique-se de que o caminho está correto
-import Formulario from './components/Formulario'; // Certifique-se de que o caminho está correto
+import './components/Condicional'
+import Condicional from './components/Condicional';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Testando os eventos</h1>
-      <Evento numero="1" />
-      <Evento numero="2" />
-      <Formulario />
+      <h1>Testando eventos condicionais</h1>
+     <Condicional/>
+  
+
     </div>
   );
 }
